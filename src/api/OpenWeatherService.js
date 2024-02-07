e18d0051b59b11d2414e5f27274ee381c7c8e1ee
Biswaraj21 <111447635+Biswaraj21@users.noreply.github.com>
2024-02-07 20:00:28 +0530

    changes by biswa

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -15,10 +15,10 @@ export async function fetchWeatherData(lat, lon) {
   try {
     let [weatherPromise, forcastPromise] = await Promise.all([
       fetch(
-        `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+        `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
       ),
       fetch(
-        `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+        `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
       ),
     ]);
 
